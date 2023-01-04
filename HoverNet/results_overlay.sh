cc422192dd527f3d2326629db9aecdf29f540994
Kdabke <75457841+Kdabke@users.noreply.github.com>
2023-01-03 17:51:10 -0800

    Update results_overlay.sh

--- HoverNet/results_overlay.sh
+++ HoverNet/results_overlay.sh
@@ -5,12 +5,12 @@
 #$ -l mem_free=50G
 #
 #$ -S /bin/bash
-#$ -e /common/bermanblab/data/private_data/POC_ROC/POC_ROC_slide_analysis/overlay_image_outputs
-#$ -o /common/bermanblab/data/private_data/POC_ROC/POC_ROC_slide_analysis/overlay_image_outputs
+#$ -e path_to_working_dir
+#$ -o path_to_working_dir
 
 
-source /home/dabkek/miniconda3/etc/profile.d/conda.sh
-conda activate /home/dabkek/miniconda3/envs/overlay_image
+
+conda activate overlay_image
 
 
 python json_analysis.py "$@"
