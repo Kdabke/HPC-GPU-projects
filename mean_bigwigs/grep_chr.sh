57937436c125599e80c479e7affb564da32637aa
Kdabke <75457841+Kdabke@users.noreply.github.com>
2023-01-03 17:57:37 -0800

    Update grep_chr.sh

--- mean_bigwigs/grep_chr.sh
+++ mean_bigwigs/grep_chr.sh
@@ -5,7 +5,7 @@
 #$ -l mem_free=10G
 #
 #$ -S /bin/bash
-#$ -e /common/bermanblab/data/private_data/POC_ROC/POC_ROC_RNA-Seq/POC_ROC_phase1_bw/bigwigs/bedgraph_HRD
-#$ -o /common/bermanblab/data/private_data/POC_ROC/POC_ROC_RNA-Seq/POC_ROC_phase1_bw/bigwigs/bedgraph_HRD
+#$ -e path_to_working_dir
+#$ -o path_to_working_dir
 
 grep -w "chr2" $1.bedgraph > $1.chr2.bedgraph
